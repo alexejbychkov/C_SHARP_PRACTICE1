0356b654a8acf57c9c290d31392f08944acf7386
aleksejbychkov <aleksej.bychkov.99@bk.ru>
2024-02-10 19:14:50 +0300

    mass-n-strings

---
+++ mass_n_strings/1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Massive
+{
+   public static void Main()
+   {
+      string s = "j7aNAmai2";
+      char[] chars = s.ToCharArray();
+      Console.WriteLine("Введенная строка: {0}", s);
+      Console.WriteLine("Массив символов: ");
+      for (int ctr = 0; ctr < chars.Length; ctr++)
+      {
+         Console.WriteLine("   {0}: {1}", ctr, chars[ctr]);
+      }
+   }
+}
---
+++ mass_n_strings/2/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите строку: ");
+string str = Console.ReadLine();
+
+string result = str.ToLower();
+
+Console.WriteLine("Результат: " + result);
